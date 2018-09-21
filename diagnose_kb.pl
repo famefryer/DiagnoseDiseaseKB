37d278fc446449ca86f668da9d18fa0e7fa94b03
dacharat <dacharat2013@gmail.com>
2018-09-21 14:20:30 +0700

    :fireworks: add diagnose_nb.pl

---
+++ diagnose_kb.pl
@@ -0,0 +1,20 @@
+
+Symptom(runny_nose) :- runny_nose, !.
+Symptom(headache) :- headache, !.
+Symptom(stomach_ache) :- stomach_ache, !.
+Symptom(fever) :- fever, !.
+Symptom(cough) :- cough, !.
+Symptom(sore_throat) :- sore_throat, !.
+Symptom(bloating) :- bloating, !.
+Symptom(abdominal_pain) :- abdominal_pain, !.
+Symptom(unknown).
+
+Medicine(tiffy) :- tiffy, !.
+Medicine(carbon) :- carbon, !.
+Medicine(tylenol) :- tylenol, !.
+Medicine(disento) :- disento, !.
+Medicine(unknown).
+
+Disease(cold) :- cold, !.
+Disease(diarrhea) :- diarrhea, !.
+Disease(unknown).
