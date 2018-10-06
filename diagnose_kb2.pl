971891cb0ce5dd37a8fdaa1b67ba3868f22a3d10
Non <non9441@gmail.com>
2018-10-06 18:25:59 +0700

    test copy animal_iden:bug:

---
+++ diagnose_kb2.pl
@@ -0,0 +1,77 @@
+/* animal.pro
+  animal identification game.  
+
+    start with ?- go.     */
+
+go :- hypothesize(Disease),
+      write('You might have this disease: '),
+      write(Disease),
+      nl,
+      undo.
+
+
+/* hypotheses to be tested */
+hypothesize(cold)     :- cold, !.
+hypothesize(diarrhea) :- diarrhea, !.
+hypothesize(flu)      :- flu, !.
+hypothesize(cholera)  :- cholera, !.
+hypothesize(unknown).             /* no diagnosis */
+
+/* animal identification rules */
+flu :- cold,
+       verify(body_aches).
+cholera :- diarrhea,
+           verify(nausea).
+
+/* classification rules */
+cold :- verify(runny_nose), !.
+cold :- verify(headache),
+        verify(fever),
+        verify(cough),
+        verify(sore_throat).
+diarrhea :- verify(watery stools), !.
+diarrhea :- verify(bloating),
+            verify(abdominal_pain),
+            verify(fever).
+# mammal    :- verify(has_hair), !.
+# mammal    :- verify(gives_milk).
+# bird      :- verify(has_feathers), !.
+# bird      :- verify(flys), 
+#              verify(lays_eggs).
+# carnivore :- verify(eats_meat), !.
+# carnivore :- verify(has_pointed_teeth), 
+#              verify(has_claws),
+#              verify(has_forward_eyes).
+# ungulate :- mammal, 
+#             verify(has_hooves), !.
+# ungulate :- mammal, 
+#             verify(chews_cud).
+
+/* how to ask questions */
+ask(Question) :-
+    write('Does you have the following symptoms: '),
+    write(Question),
+    write('? '),
+    read(Response),
+    nl,
+    ( (Response == yes ; Response == y)
+      ->
+       assert(yes(Question)) ;
+       assert(no(Question)), fail).
+
+:- dynamic yes/1,no/1.
+
+/* How to verify something */
+verify(S) :-
+   (yes(S) 
+    ->
+    true ;
+    (no(S)
+     ->
+     fail ;
+     ask(S))).
+
+/* undo all yes/no assertions */
+undo :- retract(yes(_)),fail. 
+undo :- retract(no(_)),fail.
+undo.
