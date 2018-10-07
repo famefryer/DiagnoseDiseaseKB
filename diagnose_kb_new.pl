a0f26d406c312046fec1842df97c3254248da15e
Triwith Mutitakul <triwith.m@ku.th>
2018-10-07 15:03:50 +0700

    Add more disease and symptom

--- diagnose_kb_new.pl
+++ diagnose_kb_new.pl
@@ -13,28 +13,52 @@ start :-
 
 /* hypotheses to be tested */
 hypothesize(cold)     :- cold, !.
+hypothesize(migraine) :- migraine, !.
 hypothesize(diarrhea) :- diarrhea, !.
-# hypothesize(flu)      :- flu.
-# hypothesize(cholera)  :- cholera.
+hypothesize(muscular_pain) :- muscular_pain, !.
+hypothesize(skin_scratch) :- skin_scratch, !.
+hypothesize(urticaria) :- urticaria, !.
+hypothesize(motion_sick) :- motion_sick, !.
+hypothesize(dyspepsia) :- dyspepsia, !.
 hypothesize(aphthous_ulcer):- aphthous_ulcer, !.
 hypothesize(unknown).             /* no diagnosis */
 
 /* disease identification rules */
-# flu :- cold,
-#        verify(body_aches).
-# cholera :- diarrhea,
-#            verify(nausea).
-cold :- verify(runny_nose),
-        verify(headache),
+cold :- verify(headache),
+        verify(runny_nose),
         verify(fever),
         verify(cough),
         verify(sore_throat).
+
+migraine :- verify(oneside_headache),
+            verify(throbbing_pain_headache).
 
 diarrhea :- verify(watery_stools),
             verify(bloating),
             verify(abdominal_pain),
             verify(fever).
 
+aphthous_ulcer :- verify(hurt_in_mouth),
+                  verify(hurt_in_lips),
+                  verify(hurt_in_tongue).
+
+muscular_pain :- verify(muscle_strain),
+                 verify(legache),
+                 verify(armache).
+skin_scratch :- verify(pain_at_wound),
+                verify(blood_at_wound).
+
+motion_sick :- verify(dizziness),
+               verify(vomiting).
+
+urticaria :- verify(skin_colored_welts),
+                  verify(skin_itchy).
+
+dyspepsia :- verify(upper_abdominal_pain),
+             verify(abdominal_distention),
+             verify(bloating).
+
+                      
 /* how to ask questions */
 ask(Question) :-
     write('Does you have the following symptom: '),
@@ -65,6 +89,15 @@ cureBy(cold,tiffy).
 cureBy(cold,tylenol).
 cureBy(diarrhea,carbon).
 cureBy(diarrhea,disento).
+cureBy(aphthous_ulcer,trinolone).
+cureBy(aphthous_ulcer,kanolone).
+cureBy(muscular_pain,nurofen).
+cureBy(muscular_pain,counter_pain).
+cureBy(skin_scratch,betadine).
+cureBy(motion_sick,dimenhydrinate).
+cureBy(urticaria,Chlorpheniramine).
+cureBy(migraine,triptans).
+cureBy(dyspepsia,prevacid).
 
 getMedicine(Disease) :-
     write('Medicines : (')->
